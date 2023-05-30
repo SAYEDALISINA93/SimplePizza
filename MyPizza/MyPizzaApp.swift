312991be92bd06a4b9b9b884d8ef62654ddbfef4
Alisina <sayedalisinaa@gmail.com>
2023-05-30 15:50:44 +0200

    Initial Commit

---
+++ MyPizza/MyPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPizzaApp.swift
+//  MyPizza
+//
+//  Created by Alisina on 30.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MyPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
