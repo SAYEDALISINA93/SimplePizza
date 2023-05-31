1752910dbd605dc41e2752b995abf3965b84aec3
Alisina <sayedalisinaa@gmail.com>
2023-05-31 10:32:28 +0200

    remaining files added

--- MyPizza/MenuView.swift
+++ MyPizza/MenuView.swift
@@ -5,4 +5,25 @@
 //  Created by Alisina on 30.05.23.
 //
 
-import Foundation
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        ScrollView{
+            ForEach(1...25, id:\.self){ item in
+                MenuRowView(item: item)
+            }
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
+
+
+
+
+
