91310a786f98ee07ac97f6bdf0f853dcb297b404
Alisina <sayedalisinaa@gmail.com>
2023-06-11 20:31:47 +0200

    new file added

---
+++ MyPizza/MenuGridView.swift
@@ -0,0 +1,32 @@
+//
+//  MenuGridView.swift
+//  MyPizza
+//
+//  Created by Alisina on 11.06.23.
+//
+
+import SwiftUI
+
+struct MenuGridView: View {
+    var menu: [MenuItem]
+    @State var selectedItem:MenuItem = noMenuItem
+    let columnLayout = Array(repeating: GridItem(), count: 3)
+    var body: some View {
+        VStack {
+            Text(selectedItem.name)
+            ScrollView {
+                LazyVGrid(columns: columnLayout) {
+                    ForEach(menu) { item in
+                        MenuItemTileView(menuItem: item)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MenuGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuGridView(menu: MenuModel().menu)
+    }
+}
