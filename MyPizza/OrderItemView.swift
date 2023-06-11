049e1307b33cfb26a772c6f1829d34a5f0bd86c0
Alisina <sayedalisinaa@gmail.com>
2023-06-11 20:23:07 +0200

    new changes on orderitemview

--- MyPizza/OrderItemView.swift
+++ MyPizza/OrderItemView.swift
@@ -11,17 +11,45 @@ struct OrderItemView: View {
     @Binding var orderItem: OrderItem
     @State private var quantity = 1
     @State private var doubleIngredient = false
+    @State var pizzaCrust: PizzaCrust
+    @State private var name:String = ""
+    @State private var comments:String = ""
+    init(orderItem: Binding<OrderItem>){
+        self._orderItem = orderItem
+        self.pizzaCrust = orderItem.item.crust.wrappedValue
+    }
     
     var body: some View {
         VStack{
+            TextField("Name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
             Toggle(isOn: $doubleIngredient) {
                 Text("Double Ingredients " + (doubleIngredient ? "Yes" : "No"))
                 Stepper(value: $quantity, in: 1...10){
                     Text("\(quantity) " + (quantity == 1 ? "Pizza" : "Pizzas"))
                 }
             }
+            Picker(selection: $pizzaCrust) {
+                ForEach(PizzaCrust.allCases, id: \.self){ crust in
+                    Text(crust.rawValue).tag(crust)
+                }
+            } label: {
+                Text("Pizza Crust")
+            }
+            .pickerStyle(MenuPickerStyle())
+            VStack {
+                Text("Comments")
+                    .foregroundStyle(.secondary)
+                TextEditor(text: $comments)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 3))
+            .shadow(radius: 1)
+            Spacer()
+            
         }
+        .padding()
     }
+    
 }
 
 struct OrderItemView_Previews: PreviewProvider {
